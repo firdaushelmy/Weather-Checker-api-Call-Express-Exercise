95298ce57bc574691b4f89defdb37419107bb0f3
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-05-31 23:44:58 +0800

    added api key and test if hidden api is working on server

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
+const https = require('https');
+
 require('dotenv').config();
 
 app.get('/', function (req, res) {
-  res.send('server is up and running')
+  const api_key = process.env.API_KEY;
+  const url = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=${api_key}&units=metric';
+
+  https.get(url, function (response) {
+    console.log(response);
+  })
+
+  res.send('server is up and running');
 })
 
 app.listen(3000, function () {
-  console.log('server is running on 3000')
+  console.log('server is running on 3000');
 })
