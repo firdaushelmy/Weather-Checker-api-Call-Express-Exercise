18fa31ca2101a66f4c0a32314b8bea80bf8784b7
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-06-01 23:48:53 +0800

    added body-parser, parsed in form to express, refactored code to respond to form post request

--- app.js
+++ app.js
@@ -2,34 +2,41 @@ const express = require('express');
 const app = express();
 const https = require('https');
 
+const bodyParser = require('body-parser')
 require('dotenv').config();
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html')
 })
 
-// const api_key = process.env.API_KEY;
-// const query = 'London'
-// const unit = 'metric'
-// const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${api_key}&units=${unit}`;
+app.post('/', function (req, res) {
+  const api_key = process.env.API_KEY;
+  const query = req.body.cityName
+  const unit = 'metric'
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${api_key}&units=${unit}`;
 
-// https.get(url, function (response) {
-//   console.log(response.statusCode);
+  https.get(url, function (response) {
+    console.log(response.statusCode);
 
-//   response.on('data', function (data) {
-//     const weatherData = JSON.parse(data);
-//     const temp = weatherData.main.temp;
-//     const weatherIcon = weatherData.weather[0].icon
-//     const imageURL = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`
-//     console.log(temp)
-//     const weatherDesc = weatherData.weather[0].description
-//     res.write(`<h1>The temperature in London is ${temp} degree celcius</h1>`);
-//     res.write(`<h2>The weather looks like ${weatherDesc}</h2>`);
-//     res.write(`<img src=${imageURL}>`)
+    response.on('data', function (data) {
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const weatherIcon = weatherData.weather[0].icon
+      const imageURL = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`
+      console.log(temp)
+      const weatherDesc = weatherData.weather[0].description
+      res.write(`<h1>The temperature in ${req.body.cityName} is ${temp} degree celcius</h1>`);
+      res.write(`<h2>The weather looks like ${weatherDesc}</h2>`);
+      res.write(`<img src=${imageURL}>`)
 
-//     res.send();
-//   })
-// })
+      res.send();
+    })
+  })
+})
+
+
 
 
 
