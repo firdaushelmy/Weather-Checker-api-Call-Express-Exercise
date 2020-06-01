ef65ca451ee6f8fa5a3ca86a281e237aa53937a8
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-06-01 23:56:17 +0800

    deleted some leftover code and added readme

--- app.js
+++ app.js
@@ -18,14 +18,11 @@ app.post('/', function (req, res) {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${api_key}&units=${unit}`;
 
   https.get(url, function (response) {
-    console.log(response.statusCode);
-
     response.on('data', function (data) {
       const weatherData = JSON.parse(data);
       const temp = weatherData.main.temp;
       const weatherIcon = weatherData.weather[0].icon
       const imageURL = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`
-      console.log(temp)
       const weatherDesc = weatherData.weather[0].description
       res.write(`<h1>The temperature in ${req.body.cityName} is ${temp} degree celcius</h1>`);
       res.write(`<h2>The weather looks like ${weatherDesc}</h2>`);
@@ -35,10 +32,6 @@ app.post('/', function (req, res) {
     })
   })
 })
-
-
-
-
 
 app.listen(3000, function () {
   console.log('server is running on 3000');
